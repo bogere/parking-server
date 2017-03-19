585b4b3e0a4d4b897b7d7fb4ce348586f4656e23
goldsoft25 <goldsoft25@yahoo.com>
2017-03-19 17:00:35 +0300

    debugging SQL errors in progress

--- parking/routes/parking.js
+++ parking/routes/parking.js
@@ -2,6 +2,7 @@
 //////using the router level middleware... router.get()... seperate functionality.
 var express = require('express');
 var  mysql = require('mysql');
+var md5 = require('md5'); //hasing messages.
 var router = express.Router();
 
 //create connection to the localhost.
@@ -17,12 +18,14 @@ connection.connect(function(err){
      console.error('Error connecting', err.stack);
      return;
   }
-  console.log('Successfully connected to the  db..')
+  //console.log('Successfully connected to the  db..'); //or use the else.. block to stop teh app from crashing
+   console.log('Connected as id', connection.threadId);
 }); //make connection.  export it as module..dbconnection.
 
 router.get('/', function(req,res,next){
-  res.send('Accessing the Parking home page');
-    //console.log('Accessing the parking data'); //parking slot..
+    res.send('Accessing the Parking home page');
+  //setting the header in JSOn format.
+     //res.setHeader('Content-Type', 'application/*+json'); //Error: Can't set headers after they are sent.
     //interact with the db... return the parking data.
     //var sql = "SELECT * FROM 'task'"; //Error: ER_PARSE_ERROR: You have an error in your SQL syntax;
     var sql = "SELECT * FROM task";
@@ -32,9 +35,14 @@ router.get('/', function(req,res,next){
           console.log(err);
        } else {
         //  res.json(rows); //what about JSON.Stringify()...
-        console.log(rows)
+          console.log(JSON.stringify(rows));  //converts them into JSON perfectly.
+         //res.end(JSON.stringify(rows)); //no data retrieved.
+          //res.send(JSON.stringify(rows));
        }
-    })
+    });
+    connection.end(); //Closing the connection ...ensures all remaining queries r
+                    //executed  b4 sending quit packet to  SQL server. terminate  connection gracefully
+
     next()
 });
 
@@ -42,6 +50,32 @@ router.get('/signup', function(req,res,next){ //if lacks req,res parameters.. th
    res.send('Registering in progress');
   console.log('Iam registering one of the drivers')
   next();
+});
+
+//for registering..
+router.post('/register', function(req,res,next){
+     //capture the values posted... VehicleNo n PIN or password. from the form.
+         var vehicleNo =   req.body.vehicleNo,
+                //  pass =   req.body.password;
+                //pass = md5(req.body.password); //hash the password...not efficient..
+                pass = req.body.password;
+
+        //insert the values into the database..... register the user/car. neatly escaping queries... SQL injection.
+          var carDetails = {vehicleNo: vehicleNo, vehiclePass: pass}; //caters for table columsn n its values.
+         //var carDetails = {vehicle: vehicleNo, vehiclePass: md5(pass)};
+
+         connection.query('INSERT INTO vehicle SET ?', carDetails,  function(err,rows){
+                            //? ==> escaping queries.. stop SQL injection.
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Successfully registered', JSON.stringify(rows)); //difference btn rows n results.
+                }
+
+         });
+
+
+ next()
 })
 
 module.exports = router; // this makes all the routes(/ or /register) defined  available in object.
