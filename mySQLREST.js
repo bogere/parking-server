c963d8483d3a798153ba0b9fadf9f499b0df340d
goldsoft25 <goldsoft25@yahoo.com>
2017-04-15 00:43:43 +0300

    finsihed teh REST API with stubborn login

--- mySQLREST.js
+++ mySQLREST.js
@@ -2,7 +2,8 @@
 //I'm trying to create a REST service which deals with databases with restify and mysql.
 var restify = require('restify');
 var mysql = require('mysql');
-var md5 = require('md5');
+//var md5 = require('md5');
+var bcrypt = require('bcrypt');
 
 var server = restify.createServer({
   name: 'Parking Mgt Server',
@@ -13,6 +14,12 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+// I hear that restify borrows alot from express.. like express for only REST..
+/*server.use(function(req,res,next){ //allow to  serve cross domain requests
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+}) */
+
 
 //Empty db info on purpose
 var pool = mysql.createPool({ //when using pools better vesrion.
@@ -22,6 +29,16 @@ var pool = mysql.createPool({ //when using pools better vesrion.
     password : 'Bob100',
     database : 'rest'
 });
+
+//work on encryptiion technique... use a function...
+//testing for password encryption...
+/*var password = "Hello this z goldsoft!!!!";
+var salt = bcrypt.genSaltSync(5);//create a password salt.
+// Salt and hash password
+var passwordToSave = bcrypt.hashSync(password, salt)
+console.log(passwordToSave);
+*/
+var salt = bcrypt.genSaltSync(10); //create password salt of 5... instead of 10
 
 //to stop the server from crashing when it cant establish connectio to MYSQL.
 /* pool.connect(function(err){
@@ -54,8 +71,21 @@ server.post('/register', function(req,res,next){
   //capture teh posted values.
   var vehicleNo = req.params.vehicleNo,
        pass = req.params.password;
-    var carDetails = {vehicleNo: vehicleNo, vehiclePass: md5(pass)}
-      console.log(carDetails)
+    //work on encrypting the password supplied....  later write it as function
+    //var salt = bcrypt.genSaltSync(10); //create password salt of 5... instead of 10
+    //salt n hash the password.
+    var passwordToSave = bcrypt.hashSync(pass, salt); //started hashing password on vehicleNo... 8
+        //what about the asynchronous nature... for many passwords..scalability.
+      /*  bcrypt.hash(req.body.password, 5, function( err, bcryptedPassword) {
+
+          var users={
+             "email":req.body.email,
+             "password":bcryptedPassword,
+            "created":today,
+         });*/
+    //var carDetails = {vehicleNo: vehicleNo, vehiclePass: md5(pass)}
+    var carDetails = {vehicleNo:vehicleNo, vehiclePass:passwordToSave};
+      //console.log(carDetails)
       //make connection to the db.... to perform SQL insertion
     pool.getConnection(function(err,connection){
         connection.query('INSERT INTO vehicle SET ?', carDetails, function(err,rows){
@@ -76,25 +106,97 @@ server.post('/register', function(req,res,next){
 });
 
 //for loggin.... the user
-server.post('/login', function(req,res){
+/*server.post('/login', function(req,res){
   // capture login creentials from login form
-  var vehicleNo = req.params.vehicleNo,
-       pass = req.params.password;
+  var vehicleNoInput = req.params.vehicleNo,
+       passInput = req.params.password;
 
   //make connection to the DB n compare the results to posted values.
   pool.getConnection(function(err,connection){
        //var sql = "SELECT * FROM vehicle WHERE vehicleNo = ? AND vehiclePass = ?"
-       var sql = "SELECT * FROM vehicle WHERE vehicleNo = ? AND md5(vehiclePass)= ?"
        connection.query(sql ,[vehicleNo, pass], function(err,count){
-          /* if (err) {
-              res.json(err);
-           } else {
-               res.json(count)
-           }*/
+
            if (err) throw err;
            res.send(count);
-       })
+       })*/
+    //utilize the bcrypt login script instead of md5..
+    /*   connection.query("SELECT * FROM vehicle WHERE vehicleNo = ?",
+                          [vehicleNoInput],
+                    function(err,rows){
+                    /*  if (err) {
+                          //return done(err);
+                          res.json({"Message": "Error happened"});
+                      }
+                      //well the username exists.. thus compare the passwords in db n for that of input
+                      if (bcrypt.hashSync(passInput,salt)=== rows[0].vehiclePass) {
+                        // if (bcrypt.hashSync(passwordEnteredByUser, salt) === rows[0].password) {
+                            res.json({"Message": "Success"});
+                      }*/
+                      /*  if(err)throw err;
+                        bcrypt.hashSync(passInput,salt)=== rows[0].vehiclePass){
+                            res.json({"Message": "Success"});
+                        }
+                    }
+     );
   })
+}) */
+
+//Login script that z going to work out.... but that z too much code samples looking
+server.post('/login', function(req,res,next){
+  // capture login creentials from login form
+      var vehicleNoInput = req.params.vehicleNo, //vehicleNo is not defined
+       passInput = req.params.password;
+      //make the interactions with the DB... compare above values with DB results...
+      pool.getConnection(function(err,connection){
+        //selects user details from the DB... n compare the results.
+        //thanx to http://technoetics.in/tutorials/handling user registration n login
+        connection.query('SELECT * FROM vehicle WHERE vehicleNo = ?', [vehicleNoInput],
+                  function(err,results,fields){ //callback...of db results.
+                        if (err) {
+                            // console.log("error ocurred",error);
+                            res.send({ //no SQL connection.. SQL problems.
+                                "code": 400,
+                                "failed":"error ocurred"
+                            });
+                        } else {
+                           //// console.log('The solution is: ', results);
+                           //if no err, then check whether the email/user exists
+                           if (results.length>0) {
+                                //then check password for allowing successful login.
+                                    /* if ([0].vehiclePass== passInput) { //compare with password supplied
+                                          res.send({
+                                            "code": 200,
+                                            "success": "login successful"
+                                          })*/
+                                    //what abbout decrypting the hashed password from the DB..
+                                    //bcrypt.compare(password, results[0].password, function(err, doesMatch){
+                                    bcrypt.compare(passInput, results[0].vehiclePass, function(err,doesMatch){
+                                        if(doesMatch){
+                                           res.send({
+                                             "code": 200,
+                                             "success": "login successful"
+                                           });
+                                      //  }
+
+                                         } else {
+                                          res.send({
+                                             "code": 204,
+                                            "success":"Email and password does not match"
+                                          });
+
+                                        }
+                                    });
+
+                            } else {
+                              res.send({
+                                "code":204,
+                                "success":"Email does not exist"
+                              })
+                           }
+                        }
+
+             })
+      })
 })
 
 //reserve the parking area... update teh parkig_status table
